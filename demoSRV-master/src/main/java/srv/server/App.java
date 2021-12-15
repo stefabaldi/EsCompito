59707ce300a902d237b9d906bd8ec3802a506001
lai2-01 <lai2-01@itismeucci.com>
2021-12-15 12:42:19 +0100

    added

---
+++ demoSRV-master/src/main/java/srv/server/App.java
@@ -0,0 +1,12 @@
+package srv.server;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Server server1 = new Server();
+        server1.attendi();
+        server1.comunica();
+    }
+}
---
+++ demoSRV-master/src/main/java/srv/server/Server.java
@@ -0,0 +1,88 @@
+package srv.server;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+
+    ServerSocket server = null;
+    Socket client = null;
+    String StringRV = null;
+    String StringMD = null;
+    int Biglietti= 5;
+    BufferedReader inDalClient;
+    DataOutputStream outVersoClient;
+    public Server(){
+        
+    }
+    public Socket attendi()
+    {//NON SI TOCCA
+        try 
+        {
+
+            String ind = InetAddress.getLocalHost().getHostAddress();
+            System.out.println("Server in esecuzione..."+ind);
+            server = new ServerSocket(6789);
+            client = server.accept();
+            server.close();
+            inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            outVersoClient = new DataOutputStream( client.getOutputStream());
+        } 
+        catch (Exception e) 
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Errore durante l'istanza del server");
+            System.exit(1);
+        }
+        return client;
+    }
+
+    public void comunica() 
+    {
+        
+        try
+        {
+        outVersoClient.writeBytes(" Benvenuto utente, inviare una richiesta di disponibilità di un biglietto o per il suo acquisto" +'\n');
+        for(;;)
+        {
+        StringRV = inDalClient.readLine();
+        
+        
+
+        
+
+        if(Biglietti>0)
+        {
+            if(StringRV.equals("D"))
+            {
+                StringMD= " Sono rimasti: " +Biglietti+ " biglietti";
+                outVersoClient.writeBytes(StringMD+'\n');
+            }
+            else if(StringRV.equals("A"))
+            {
+                StringMD=" Biglietto acquistato ";
+                Biglietti--;
+                outVersoClient.writeBytes(StringMD+'\n');
+            }
+        }
+        else
+        {
+            outVersoClient.writeBytes((" Spiacenti, i biglietti sono terminati")+'\n');
+
+            System.out.println(" Server: elaborazione conclusa ...");
+            client.close();
+        }
+        
+
+        
+    }
+        }
+        catch(Exception e)
+        {
+            
+        }
+        
+    }
+}
